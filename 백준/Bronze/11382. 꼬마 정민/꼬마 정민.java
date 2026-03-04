ce1baa9f3b9c613e2c5d830dd2e7ef3365c7c7f0
이승준 <72748734+HiLeeS@users.noreply.github.com>
2026-03-04 18:51:11 +0900

    [Bronze V] Title: 꼬마 정민, Time: 104 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        long sum = 0;
+        for(int i = 0; i < 3; i++){
+            sum += Long.parseLong(st.nextToken());
+        }
+        
+        System.out.print(sum);
+    }
+}
