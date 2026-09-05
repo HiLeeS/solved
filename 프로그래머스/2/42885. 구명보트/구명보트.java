063660e8a57eadd31ba3907dd8adaf2cc5754adc
이승준 <72748734+HiLeeS@users.noreply.github.com>
2026-09-05 13:45:11 +0900

    [level 2] Title: 구명보트, Time: 10.85 ms, Memory: 59.3 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,32 +1,28 @@
-// 첫 생각 : 투 포인터로 정렬된 people에서 당겨오면서 양 포인터의 합이 limit보다 작으면 answer 1 추가 ?
 import java.util.*;
-
 class Solution {
     public int solution(int[] people, int limit) {
+        
+        
         int answer = 0;
         
-        Arrays.sort(people);    //투 포인터를 위해 정렬
+        Arrays.sort(people);
         
-        int start = 0;  //시작 위치
-        int end = people.length-1;  //마지막 위치
+        int left = 0;
+        int right = people.length - 1;
         
-        
-        while(start < end){ //두 포인터가 겹치기 전까지 반복
-            int sum = people[start] + people[end];
-            if(sum <= limit) {  //보트에 탈 수 있으면
+        while(left <= right){
+            if(people[right] + people[left] <= limit){
                 answer++;
-                people[start] = 0;
-                people[end] = 0;
-                start++;
-                end--;
+                left++;
+                right--;
             }
-            else if(sum > limit){   //보트 제한 무게보다 높으면 end를 당김
-                end--;
+            
+            else{
+                answer++;
+                right--;
             }
-        }
-        
-        for(int i = 0; i < people.length; i++){ //혼자 보트를 타야하는 경우
-            if(people[i] != 0) answer++;
+            
+            
         }
         
         
